be169653577098e5ce61b5ff096d564ec597618e
BongWonHee <leeban1004@gmail.com>
2023-08-01 17:08:58 +0900

    first commit

---
+++ datatype_strings.py
@@ -0,0 +1,13 @@
+str ='yojulab !'
+len(str)
+pass
+
+# len(str)
+# 9
+# "ju" in str
+# True
+# "Not" in str
+# False
+# "Not" not in str
+# True
+# https://www.w3schools.com/python/default.asp
---
+++ syntaxs.py
@@ -0,0 +1,7 @@
+# tab and 변수사용
+first = 2
+second = 5
+
+if first > second:
+  print("Five is greater than two!")
+print('yojulab!')

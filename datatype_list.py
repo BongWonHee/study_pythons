d038d57bd4da3205c9235d8201d8bb84d931da4e
BongWonHee <leeban1004@gmail.com>
2023-08-01 18:23:08 +0900

    23.08.01 수업종료

---
+++ datatype_list.py
@@ -0,0 +1,22 @@
+# slicing
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+# type(thislist) 타입확인.
+# <class 'list'>
+# len(thislist)
+# 7
+# thislist[1:3] 1부터 3전까지확인
+# ['banana', 'cherry']
+# thislist[:-1] 뒤에서 1자리빼고 확인
+# ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon']
+
+# change value in list 
+# thislist[1] = 'watermelon' thislist 내부 인덱스 변경
+# thislist[1:3] = ['cherry', 'watermelon'] 내부 인덱스2개 변경
+
+# thislist.sort() 내부 인덱스 정렬
+# thislist.sort(reverse=True) 내부 인덱스 역정렬
+pass
+
+
+
--- datatype_strings.py
+++ datatype_strings.py
@@ -1,13 +1,42 @@
-str ='yojulab !'
-len(str)
+string ='yojulab !'
+len(string)
 pass
 
-# len(str)
+# len(string)
 # 9
-# "ju" in str
+# "ju" in string
 # True
-# "Not" in str
+# "Not" in string
 # False
-# "Not" not in str
+# "Not" not in string
 # True
 # https://www.w3schools.com/python/default.asp
+
+#slicing 
+string[3]
+pass
+# 순열, serise
+# string[3:6] 3번자리부터 6번까지
+# 'ula'
+# string[3:] 3번자리부터 끝까지
+# 'ulab !'
+# string[:-2] 뒤에서 2자리 잘라내기
+# 'yojulab'
+
+# format 타입이 다른 2개이상의 변수를 결합할때는 casting 해야한다.
+
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+pass
+# txt_second
+# 'My name is John, I am {}.'
+# txt_second.format(age)
+# 'My name is John, I am 36.'
+quantity = 3    # index 0
+itemno = 567    # index 1
+price = 49.95   # index 2
+myorder = "I want {0} pieces of item {1} for {2} dollars. I have just {2}."
+myorder.format(quantity, itemno, price)
+# 'I want 3 pieces of item 567 for 49.95 dollars. I have just 49.95.'
+pass

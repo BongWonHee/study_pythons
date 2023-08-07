a2bfd7f36f065257ca38ae1f93fddbe1afca742a
BongWonHee <leeban1004@gmail.com>
2023-08-07 10:12:03 +0900

    날짜계산

--- datatype_datetimes.py
+++ datatype_datetimes.py
@@ -1,4 +1,12 @@
 import datetime
 # print today 
 today =  datetime.datetime.now()
+someday = datetime.datetime(2023,8,3)
+# 50578/(24*60*60)
+# 0.5853935185185185
+
+# startday = datetime.datetime(2023,5,8)
+# someday = today - startday
+# someday.days
+# 91
 pass
